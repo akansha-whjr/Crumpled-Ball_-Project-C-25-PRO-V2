5e5731b4a125f398383f5cddb81bd5fb15c4e4fb
akansha-whjr <77827780+akansha-whjr@users.noreply.github.com>
2021-05-05 17:29:21 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,23 @@
+class paper{
+    constructor(x,y,r){
+        var options={
+        density:1.2,
+        friction:0.5,
+        restitution:0.3,
+        isStatic:false
+        }
+        this.x=x
+        this.y=y
+        this.r=r
+        this.image=loadImage("paper.png")
+        this.body=Bodies.circle(this.x,this.y,this.r,options)
+        World.add(world,this.body)
+    }
+    display(){
+        var pos=this.body.position
+        push ()
+        translate(pos.x,pos.y)
+        image(this.image,0,0,this.r,this.r)
+        pop()
+    }
+}
